63cae9a10c1861eff6f156b06053151a348f045f
Charlotte S <93612273+cbsumo305@users.noreply.github.com>
2022-03-15 19:43:36 -0500

    First complete draft of portfolio

---
+++ src/Project.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+const Project = props => {
+
+    return (
+        <article className="project" key={props.project.id}>
+
+            <figure>
+                <img alt="project-image" src={props.project.thumbnail} />
+            </figure>
+
+            <h4>{props.project.title}</h4>
+
+            <p className="description">{props.project.description}</p>
+            <p><strong>Skills:</strong> {props.project.skills.join(", ")}</p>
+            <p><strong>Tools:</strong> {props.project.tools.join(", ")}</p>
+
+            <a href={ props.project.link } target="_blank">Live Page</a>
+        </article>
+
+    )
+}
+
+export default Project;
---
+++ src/ProjectData.js
@@ -0,0 +1,49 @@
+const projects = [
+    {
+        id: 0,
+        title: "Sticky Notes",
+        link: "https://b39mz3.csb.app/",
+        thumbnail: "../img/sticky-notes-thumbnail.png",
+        skills: ["ES6", "JSX"],
+        tools: ["CodeSandbox"],
+        description: "This interactive app lets users create, delete, and modify notes on their computer to track to-do items."
+    },
+    {
+        id: 1,
+        title: "Unplugged Retreat",
+        link: "https://cbsumo305.github.io/unplugged-retreat/",
+        thumbnail: "../img/guess-the-word.png",
+        skills: ["HTML5", "CSS3", "Responsive Typography"],
+        tools: ["Photoshop", "CodeSandbox"],
+        description: "This responsive 3-page website was built from a design comp and adapts to mobile, tablet, and desktop screens."
+    },
+    {
+        id: 2,
+        title: "Guess the Word",
+        link: "https://cbsumo305.github.io/guess-the-word/",
+        thumbnail: "../img/guess-the-word.png",
+        skills: ["JavaScript"],
+        tools: ["CodeSandbox", "GitHub"],
+        description: "Players guess the word by entering one letter at a time. If the player guesses all the letters correctly before they use up their allotted guesses, they win!"
+    },
+    {
+        id: 3,
+        title: "",
+        link: "",
+        thumbnail: "",
+        skills: ["ES6", "JSX"],
+        tools: ["CodeSandbox"],
+        description: ""
+    },
+    {
+        id: 4,
+        title: "",
+        link: "",
+        thumbnail: "",
+        skills: ["ES6", "JSX"],
+        tools: ["CodeSandbox"],
+        description: ""
+    }
+];
+
+export default projects;
