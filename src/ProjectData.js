5e79f267c5ce2a3887730b895384eb54453c2921
Charlotte S <93612273+cbsumo305@users.noreply.github.com>
2022-03-16 00:06:57 -0500

    Fixed broken image links

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -3,7 +3,7 @@ const projects = [
         id: 0,
         title: "Sticky Notes",
         link: "https://b39mz3.csb.app/",
-        thumbnail: "../img/sticky-notes-thumbnail.png",
+        thumbnail: "img/sticky-notes-thumbnail.png",
         skills: ["ES6", "JSX"],
         tools: ["CodeSandbox"],
         description: "This interactive app lets users create, delete, and modify notes on their computer to track to-do items."
@@ -12,7 +12,7 @@ const projects = [
         id: 1,
         title: "Unplugged Retreat",
         link: "https://cbsumo305.github.io/unplugged-retreat/",
-        thumbnail: "../img/unplugged-thumbnail.png",
+        thumbnail: "img/unplugged-thumbnail.png",
         skills: ["HTML5", "CSS3", "Responsive Typography"],
         tools: ["Photoshop", "CodeSandbox"],
         description: "This responsive 3-page website was built from a design comp and adapts to mobile, tablet, and desktop screens."
@@ -21,7 +21,7 @@ const projects = [
         id: 2,
         title: "Guess the Word",
         link: "https://cbsumo305.github.io/guess-the-word/",
-        thumbnail: "../img/guess-the-word-thumbnail.png",
+        thumbnail: "img/guess-the-word-thumbnail.png",
         skills: ["JavaScript"],
         tools: ["CodeSandbox", "GitHub"],
         description: "Players guess the word by entering one letter at a time. If the player guesses all the letters correctly before they use up their allotted guesses, they win!"
